e0d75c77fa0ce862436e88b949315556ac700c17
Richard Morris <rallanmorris@gmail.com>
2021-01-15 16:21:45 -0500

    Added Rocket model and basic rocket.cs movement script

---
+++ Assets/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))//Can thrust while rotating
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        //Cannot rotate both ways at the same time
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-(Vector3.forward));
+        }
+    }
+}
